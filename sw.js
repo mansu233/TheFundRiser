f38f37d1da755e27c234e5881d7fb81f7e7d51e1
mansu233 <mansu233@gmail.com>
2026-08-25 06:14:16 +0530

    update

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
 const CACHE_NAME = 'manskit-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Try network first, fallback to cache if offline
   event.respondWith(
     fetch(event.request)
       .then((response) => {
